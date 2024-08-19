fc439b80974f8ed5e0b634892ae4e0f23fe2d1f7
naomik92 <naomik092@gmail.com>
2024-08-19 20:35:32 +0900

    cal.rbを再修正しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 require 'optparse'
-options = ARGV.getopts('y:', 'm:')
-
 require 'date'
+options = ARGV.getopts('y:', 'm:')
 
 today_year = 
 if options['y'] == nil
@@ -21,25 +20,19 @@ end
 first_date = Date.new(today_year, today_month, 1)
 last_date = Date.new(today_year, today_month, -1)
 
-# カレンダーの日付部分の配列を生成
-blank = []
-if first_date.cwday != 7
-  first_date.cwday.times {blank.push("".rjust(3))}
+blanks = []
+first_date.wday.times {blanks.push("".rjust(3))}
+
+dates = (first_date..last_date).map do |date|
+  date
 end
 
-dates = []
-(first_date..last_date).map{|date|
-  dates << date.day.to_s.rjust(3)
-}
-
-blank_and_dates = blank + dates
-
-#　カレンダーの表示
 puts "       #{today_month}月 #{today_year}\n"
 puts " 日 月 火 水 木 金 土\n"
-blank_and_dates.each_with_index{|str, index|  
-  print str
-  if (index - 6) % 7 == 0
-    print "\n"
-  end
-}
+blanks.each do |blank|
+  print blank
+end
+dates.each do |date|
+  print date.day.to_s.rjust(3)
+  print "\n" if date.saturday?
+end
