2c79648a1b5c9dfe158f8b8c98898b3a0fa93315
naomik92 <naomik092@gmail.com>
2024-08-20 12:42:54 +0900

    cal.rbを再度修正しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,19 +3,8 @@ require 'optparse'
 require 'date'
 options = ARGV.getopts('y:', 'm:')
 
-today_year = 
-if options['y'] == nil
-  Date.today.year
-else
-  options['y'].to_i
-end
-
-today_month =
-if options['m'] == nil
-  Date.today.mon
-else
-  options['m'].to_i
-end
+today_year = options['y']&.to_i || Date.today.year
+today_month = options['m']&.to_i || Date.today.mon
 
 first_date = Date.new(today_year, today_month, 1)
 last_date = Date.new(today_year, today_month, -1)
@@ -23,16 +12,10 @@ last_date = Date.new(today_year, today_month, -1)
 blanks = []
 first_date.wday.times {blanks.push("".rjust(3))}
 
-dates = (first_date..last_date).map do |date|
-  date
-end
-
 puts "       #{today_month}月 #{today_year}\n"
 puts " 日 月 火 水 木 金 土\n"
-blanks.each do |blank|
-  print blank
-end
-dates.each do |date|
+print blanks.join
+(first_date..last_date).each do |date|
   print date.day.to_s.rjust(3)
   print "\n" if date.saturday?
 end
