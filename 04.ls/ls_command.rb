4737c7f78a1c54afa61decd5084e99e27876ff65
naomik92 <naomik092@gmail.com>
2024-09-15 07:50:25 +0900

    1行に出力したいインデックス番号を、別の二次元配列に格納(すべての行)

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -6,13 +6,14 @@ contain_files.delete_if do |file|
   file.match(/^\.$|^\..+/)
 end
 
-# # copy_layout = all_layout.dup
+# 1行に出力したいインデックス番号を、別の二次元配列に格納(すべての行)
 col_numbers = 3
 row_numbers = (contain_files.size - 1).div(col_numbers) + 1
-row_countdown = row_numbers
 all_layout = []
 
-row_countdown -= 1
-# copy_layout = all_layout.dup
-row_layout = (1..contain_files.size).select { |int| int % row_numbers == row_countdown }
-all_layout.push(row_layout)
+(row_numbers - 1).downto(0) do |x|
+  row_layout = (1..contain_files.size).select { |int| int % row_numbers == (row_numbers - x) % row_numbers }
+  all_layout.push(row_layout)
+end
+
+p all_layout
