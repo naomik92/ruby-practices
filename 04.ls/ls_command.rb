bcb6cc3f48321b0d25e700f6201030f64b514ed5
naomik92 <naomik092@gmail.com>
2024-09-15 20:31:33 +0900

    lsコマンド(オプションなし)の機能を作成

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -1,19 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+COL_NUMBERS = 3
+
 contain_files = Dir.entries('.').sort
+
 contain_files.delete_if do |file|
   file.match(/^\.$|^\..+/)
 end
 
-# 1行に出力したいインデックス番号を、別の二次元配列に格納(すべての行)
-col_numbers = 3
-row_numbers = (contain_files.size - 1).div(col_numbers) + 1
-all_layout = []
+max_file_length = 0
+contain_files.each do |file|
+  max_file_length = file.bytesize if file.bytesize >= max_file_length
+end
 
-(row_numbers - 1).downto(0) do |x|
-  row_layout = (1..contain_files.size).select { |int| int % row_numbers == (row_numbers - x) % row_numbers }
+row_numbers = (contain_files.size - 1).div(COL_NUMBERS) + 1
+all_layout = []
+row_numbers.downto(1) do |x|
+  row_layout = (0..contain_files.size - 1).select { |int| int % row_numbers == (row_numbers - x) }
   all_layout.push(row_layout)
 end
 
-p all_layout
+all_layout.each do |row|
+  row.each do |index_number|
+    print contain_files[index_number].ljust(max_file_length + 1)
+  end
+  print "\n"
+end
