74c2cb4431eb44a2af461cbf45d1c932b64f69ef
naomik92 <naomik092@gmail.com>
2024-09-16 10:21:00 +0900

    メソッドを定義

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -3,27 +3,37 @@
 
 COL_NUMBERS = 3
 
-contain_files = Dir.entries('.').sort
-
-contain_files.delete_if do |file|
-  file.match(/^\.$|^\..+/)
+def find_all_files
+  Dir.entries('.').sort
 end
 
-max_file_length = 0
-contain_files.each do |file|
-  max_file_length = file.bytesize if file.bytesize >= max_file_length
-end
-
-row_numbers = (contain_files.size - 1).div(COL_NUMBERS) + 1
-all_layout = []
-row_numbers.downto(1) do |x|
-  row_layout = (0..contain_files.size - 1).select { |int| int % row_numbers == (row_numbers - x) }
-  all_layout.push(row_layout)
-end
-
-all_layout.each do |row|
-  row.each do |index_number|
-    print contain_files[index_number].ljust(max_file_length + 1)
+def delete_hiddenfile(files)
+  files.delete_if do |file|
+    file.match(/^\.$|^\..+/)
   end
-  print "\n"
 end
+
+def align_columns(files)
+  max_file_length = 0
+  files.each do |file|
+    max_file_length = file.bytesize if file.bytesize > max_file_length
+  end
+
+  row_numbers = (files.size - 1).div(COL_NUMBERS) + 1
+  all_layout = []
+  row_numbers.downto(1) do |rest_rows|
+    row_layout = (0..files.size - 1).select { |int| int % row_numbers == (row_numbers - rest_rows) }
+    all_layout.push(row_layout)
+  end
+
+  all_layout.each do |row_layout|
+    row_layout.each do |index_number|
+      print files[index_number].ljust(max_file_length + 1)
+    end
+    print "\n"
+  end
+end
+
+all_files = find_all_files
+display_files = delete_hiddenfile(all_files)
+align_columns(display_files)
