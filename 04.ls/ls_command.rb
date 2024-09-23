35650ef708e11db15185641fbd76aa08bcea5e72
naomik92 <naomik092@gmail.com>
2024-09-24 07:24:56 +0900

    命名を修正、mainメソッドの追加

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -1,39 +1,38 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-COL_NUMBERS = 3
+COL_COUNT = 3
+
+def main
+  all_files = find_all_files
+  visible_files = delete_hiddenfile(all_files)
+  display_files(visible_files)
+end
 
 def find_all_files
   Dir.entries('.').sort
 end
 
 def delete_hiddenfile(files)
-  files.delete_if do |file|
-    file.match(/^\.$|^\..+/)
-  end
+  files.reject { |file| file.start_with?(/\./) }
 end
 
-def align_columns(files)
-  max_file_length = 0
-  files.each do |file|
-    max_file_length = file.bytesize if file.bytesize > max_file_length
+def display_files(files)
+  max_file_length = files.flatten.map(&:bytesize).max
+  row_count = (files.size - 1).div(COL_COUNT) + 1
+  rows_order = []
+
+  row_count.downto(1) do |n|
+    files_order = (0..files.size - 1).select { |i| i % row_count == (row_count - n) }
+    rows_order.push(files_order)
   end
 
-  row_numbers = (files.size - 1).div(COL_NUMBERS) + 1
-  all_layout = []
-  row_numbers.downto(1) do |rest_rows|
-    row_layout = (0..files.size - 1).select { |int| int % row_numbers == (row_numbers - rest_rows) }
-    all_layout.push(row_layout)
-  end
-
-  all_layout.each do |row_layout|
-    row_layout.each do |index_number|
+  rows_order.each do |files_order|
+    files_order.each do |index_number|
       print files[index_number].ljust(max_file_length + 1)
     end
     print "\n"
   end
 end
 
-all_files = find_all_files
-display_files = delete_hiddenfile(all_files)
-align_columns(display_files)
+main
