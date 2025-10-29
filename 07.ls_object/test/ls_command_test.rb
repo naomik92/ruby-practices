683b90f778a726b7b4bf5bf99f25a8b947067d10
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-10-29 22:04:16 +0900

    テストの作成、テスト用ファイルの作成

--- 07.ls_object/test/ls_command_test.rb
+++ 07.ls_object/test/ls_command_test.rb
@@ -3,12 +3,46 @@
 
 require 'minitest/autorun'
 require_relative '../ls_command'
-require_relative '../files_table'
-require_relative '../files_list'
 
 class LsCommandTest < Minitest::Test
   def test_non_option
-    files = Dir.entries('../').sort
-    LsCommand.new(files).build_files
+    options = {}
+    files = Dir.entries('.').sort.reject
+    expected_files_array = [['abc.txt', 'ghi.txt', 'ls_command_test.rb'], ['def.txt', 'jk.txt', '']]
+    actual_files_array = LsCommand.new(files, options).build_files.map { |row| row.map(&:rstrip) }
+    assert_equal expected_files_array, actual_files_array
+  end
+
+  def test_a_option
+    options = { a: true }
+    files = Dir.entries('.').sort.reject
+    expected_files_array = [['.', 'def.txt', 'ls_command_test.rb'], ['..', 'ghi.txt', ''], ['abc.txt', 'jk.txt', '']]
+    actual_files_array = LsCommand.new(files, options).build_files.map { |row| row.map(&:rstrip) }
+    assert_equal expected_files_array, actual_files_array
+  end
+
+  def test_r_option
+    options = { r: true }
+    files = Dir.entries('.').sort.reject
+    expected_files_array = [['ls_command_test.rb', 'ghi.txt', 'abc.txt'], ['jk.txt', 'def.txt', '']]
+    actual_files_array = LsCommand.new(files, options).build_files.map { |row| row.map(&:rstrip) }
+    assert_equal expected_files_array, actual_files_array
+  end
+
+  def test_l_option
+    options = { l: true }
+    files = Dir.entries('.').sort.reject
+    expected_files_array = [['total 8'], ['-', 'rw-r--r--'], ['-', 'rw-r--r--'], ['-', 'rw-r--r--'], ['-', 'rw-r--r--'], ['-', 'rwxr--r--']]
+    actual_files_array = LsCommand.new(files, options).build_files.map { |row| row[0, 2] }
+    assert_equal expected_files_array, actual_files_array
+  end
+
+  def test_all_option
+    options = { a: true, r: true, l: true }
+    files = Dir.entries('.').sort.reject
+    expected_files_array = [['total 8'], ['-', 'rwxr--r--'], ['-', 'rw-r--r--'], ['-', 'rw-r--r--'],
+                            ['-', 'rw-r--r--'], ['-', 'rw-r--r--'], ['d', 'rwxr-xr-x'], ['d', 'rwxr-xr-x']]
+    actual_files_array = LsCommand.new(files, options).build_files.map { |row| row[0, 2] }
+    assert_equal expected_files_array, actual_files_array
   end
 end
